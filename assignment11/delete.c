56174574c0e183ea5562fd879a07d0e52b3f692c
Himanshu Sekhar Nayak <himanshuwindows8.1@gmail.com>
2019-11-10 11:25:07 +0530

    Updated loop by not using string library

--- assignment11/delete.c
+++ assignment11/delete.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
         char string[100];
-        int i, len, pos;
+        int i, pos;
 
         printf("Enter any word:-\n");
         scanf("%[^\n]", string);
@@ -11,12 +10,10 @@ int main()
         printf("\nThe word is:-\n");
         printf("%s", string);
 
-        len = strlen(string);
-
         printf("\nAt which position you want to delete?\n");
         scanf("%d", &pos);
 
-        for(i=pos-1; i<len; ++i)
+        for(i=pos-1; string[i] != '\0'; ++i)
                 string[i] = string[i+1];
 
         printf("\n---NEW WORD---\n");
