10b642c1b8e4c30dba19a577b57136dc122c8cf9
Himanshu40 <himanshuwindows8.1@gmail.com>
2019-11-21 18:25:43 +0530

    Updated to 2D Array for ascii.c

--- assignment13/ascii.c
+++ assignment13/ascii.c
@@ -1,16 +1,27 @@
 #include<stdio.h>
 int main()
 {
-	char string[100];
-	int i;
+	char string[10][30];
+	int i, n, j;
+
+	printf("How many lines you want to input?\n");
+	scanf("%d", &n);
 
 	printf("Enter the string:-\n");
-	scanf("%[^\n]", string);
+	fgetc(stdin);
+	for(i=0; i<n; ++i)
+		fgets(string[i], 30, stdin);
+
+	printf("\nThe string is:-\n");
+	for(i=0; i<n; ++i)
+		printf("%s", string[i]);
 
 	printf("\n---ASCII EQUIVALENT---\n");
 	printf("CHARACTER\tASCII CODE\n");
-	for(i=0; string[i] != '\0'; ++i)
-		printf("%c\t\t%d\n", string[i], string[i]);
+	for(i=0; i<n; ++i)
+		for(j=0; string[i][j] != '\0'; ++j)
+			if(string[i][j] != 10)
+				printf("%c\t\t%d\n", string[i][j], string[i][j]);
 
 	return 0;
 }
